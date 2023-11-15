87305dc781695e5542990904df5ebd1b21c11575
Vamsi9951 <86877189+Vamsi9951@users.noreply.github.com>
2023-11-15 16:32:39 +0530

    Delete 01_PLANS_API directory

--- 01_PLANS_API/src/main/java/in/ashokit/repo/PlanRepo.java
+++
@@ -1,9 +0,0 @@
-package in.ashokit.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import in.ashokit.entity.Plan;
-
-public interface PlanRepo extends JpaRepository<Plan, Integer>{
-
-}
